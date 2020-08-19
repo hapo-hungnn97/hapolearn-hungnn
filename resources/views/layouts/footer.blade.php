f1aacf6c7e7ded3595d023ee0915b937cfe82015
hapo-hungnn <hung.nn@haposoft.com>
2020-08-19 08:36:39 +0700

    laravel mix + template

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,40 @@
+<div class="footer container-fluid d-flex flex-row align-items-center mt-xl-5 mt-md-4 mt-5">
+    <div class="row block-foot">
+        <div class="menu col-md-4 col-12 order-md-2 d-flex flex-row justify-content-md-center justify-content-around">
+            <ul class="d-flex flex-column">
+                <li><a href="">Home</a></li> 
+                <li><a href="">Features</a></li> 
+                <li><a href="">Courses</a></li> 
+                <li><a href="">Blog</a></li>  
+            </ul>
+            <ul class="d-flex flex-column">
+                <li><a href="">Contact</a></li> 
+                <li><a href="">Terms of Use</a></li> 
+                <li><a href="">FAQ</a></li>
+            </ul>
+        </div>
+        <div class="logo col-md-4 col-7 order-md-1 d-flex flex-column align-items-center">
+            <img class="img-fluid" src="image/Hapo_Learn_white.png" alt="">
+            <br>
+            <span class="txt-logo">
+                Interactive lessons, "on-the-go"
+                <br>
+                practice, peer support.
+            </span>
+        </div>
+        <div class="contact col-md-4 col-5 order-md-3 d-flex align-items-lg-center align-items-md-center">
+            <a href="" class="d-flex flex-row align-items-center">
+                <img class="img-fluid pl-2" src="image/Group3.png" alt="" data-toggle="tooltip" data-placement="top" title="facebook.com/tuyen.dung.haposoft">
+            </a>
+            <a href="" class="d-flex flex-row align-items-center">
+                <img class="img-fluid ml-xl-5 ml-md-4 pl-2" src="image/Group4.png" alt="" data-toggle="tooltip" data-placement="top" title="info@haposoft.com">
+            </a>
+            <a href="" class="d-flex flex-row align-items-center">
+                <img class="img-fluid ml-xl-5 ml-md-4 pl-2" src="image/Group5.png" alt="" data-toggle="tooltip" data-placement="top" title="+84-85-645-9898">
+            </a>
+        </div>
+    </div>
+</div>
+<div class="copy-rights container-fluid d-flex align-items-center justify-content-center">
+    © 2020 HapoLearn, Inc. All rights reserved.
+</div>
---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,32 @@
+<div class="header navbar navbar-expand-sm container-fluid">
+    <div class="row block-header">
+        <div class="header-left col-xl-4 justify-content-xl-start block-left">
+            <button class="header-button navbar-toggler block-btn" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="fa fa-bars icon"></span>
+            </button>
+            <img class="img-logo img-fluid d-flex mx-auto ml-xl-2" src="image/Hapo_Learn.png" alt="">
+        </div>
+        <div class="header-right collapse navbar-collapse col-xl-8 justify-content-xl-end justify-content-md-center" id="navbarNav">
+            <ul class="menu navbar-nav mr-xl-2">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">all courses</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">list lesson</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">lesson detail</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#" data-toggle="modal" data-target="#signModal">login/register</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">profile</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
