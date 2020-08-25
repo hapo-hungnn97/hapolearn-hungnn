a1f5dee47e5db72ea35396487a2b2d8b00066c84
hapo-hungnn <hung.nn@haposoft.com>
2020-08-25 09:46:18 +0700

    .

--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -17,8 +17,8 @@ class CourseController extends Controller
     {
         $key = $request->search;
         $courses = Course::where('name', 'like','%'.$key.'%')
-                        ->orwhere('description', 'like','%'.$key.'%')
-                        ->paginate(config('variable.paginate'));
+                    ->orwhere('description', 'like','%'.$key.'%')
+                    ->paginate(config('variable.paginate'));
         return view('user.courses_list', compact('courses'));
     }
 }
