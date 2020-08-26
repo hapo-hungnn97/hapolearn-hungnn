3e69dfc5563de20b652cd7e7535fdf1c7f1b7311
hapo-hungnn <hung.nn@haposoft.com>
2020-08-26 14:48:56 +0700

    crud course

--- app/Course.php
+++ app/Course.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Course extends Model
 {
+    use SoftDeletes;
+    
     protected $fillable = [
         'name', 'description', 'image', 'times', 'price', 'quizze', 'teacher_id'
     ];
