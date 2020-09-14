806dfdb3b0aaa0a568d84058229ecae60d3df2ed
hapo-hungnn <hung.nn@haposoft.com>
2020-09-14 17:03:04 +0700

    fix convention

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -99,8 +99,7 @@ class Course extends Model
     {
         return $this->withCount(['users' => function ($que) {
                 $que->where('lesson_id', null);
-            }])
-        ->orderByDesc('users_count')
+        }])->orderByDesc('users_count')
         ->take(config('variable.course'))
         ->get();
     }
